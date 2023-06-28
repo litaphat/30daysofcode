91f0a2741cf17d2ef4ce6a5c46b2214270327057
kladarat <kladarat.lk@gmail.com>
2023-06-28 16:01:39 +0700

    init commit

---
+++ day1datatypes/Program.cs
@@ -0,0 +1,23 @@
+﻿int i = 4;
+double d = 4.0;
+string s = "HackerRank ";
+
+
+// Declare second integer, double, and String variables.
+string inputString;
+string inputInt;
+string inputDouble;
+// Read and save an integer, double, and String to your variables.
+inputInt = Console.ReadLine();
+inputDouble = Console.ReadLine();
+inputString = Console.ReadLine();
+// Print the sum of both integer variables on a new line.
+int sumInt = i + Convert.ToInt16(inputInt);
+Console.WriteLine(sumInt);
+// Print the sum of the double variables on a new line.
+double sumDouble = d + Convert.ToDouble(inputDouble);
+Console.WriteLine(sumDouble.ToString("#.0"));
+// Concatenate and print the String variables on a new line
+string resultConcat = s + (inputString ?? "");
+// The 's' variable above should be printed first.
+Console.WriteLine(s + (inputString ?? ""));
