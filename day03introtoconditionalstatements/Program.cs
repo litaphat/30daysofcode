df82ca8902aa47b8c83fc2a1c8126760f3e5563b
kladarat <kladarat.lk@gmail.com>
2023-07-04 03:10:28 +0700

    day03

---
+++ day03introtoconditionalstatements/Program.cs
@@ -0,0 +1,17 @@
+﻿int N = Convert.ToInt32(Console.ReadLine().Trim());
+if(N % 2 == 1)
+{
+    Console.WriteLine("Weird");
+}
+else if(N % 2 == 0 && 2 <= N && N <= 5)
+{
+    Console.WriteLine("Not Weird");
+}
+else if(N % 2 == 0 && 6 <= N && N <= 20)
+{
+    Console.WriteLine("Weird");
+}
+else if (N % 2 == 0 && 20 < N)
+{
+    Console.WriteLine("Not Weird");
+}
